7c1f12fe916d916a049aeacc22922db8156f068c
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-07-21 13:38:34 +1000

    :zap: adding in a depth limit to the prediction function

--- hierarchicalsoftmax/inference.py
+++ hierarchicalsoftmax/inference.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 import torch
 
 from . import nodes
@@ -10,7 +10,7 @@ class ShapeError(RuntimeError):
     """
 
 
-def greedy_predictions(prediction_tensor:torch.Tensor, root:nodes.SoftmaxNode) -> List[nodes.SoftmaxNode]:
+def greedy_predictions(prediction_tensor:torch.Tensor, root:nodes.SoftmaxNode, max_depth:Optional[int]=None) -> List[nodes.SoftmaxNode]:
     """
     Takes the prediction scores for a number of samples and converts it to a list of predictions of nodes in the tree.
 
@@ -19,6 +19,7 @@ def greedy_predictions(prediction_tensor:torch.Tensor, root:nodes.SoftmaxNode) -
     Args:
         root (SoftmaxNode): The root softmax node. Needs `set_indexes` to have been called.
         prediction_tensor (torch.Tensor): The predictions coming from the softmax layer. Shape (samples, root.layer_size)
+        max_depth (int, optional): If set, then it only gives predictions at a maximum of this number of levels from the root.
 
     Returns:
         List[nodes.SoftmaxNode]: A list of nodes predicted for each sample.
@@ -36,9 +37,15 @@ def greedy_predictions(prediction_tensor:torch.Tensor, root:nodes.SoftmaxNode) -
 
     for predictions in prediction_tensor:
         node = root
+        depth = 1
         while (node.children):
             prediction_chlid_index = torch.argmax(predictions[node.softmax_start_index:node.softmax_end_index])
             node = node.children[prediction_chlid_index]
+
+            # Stop if we have reached the maximum depth
+            if max_depth and depth >= max_depth:
+                break
+            depth += 1
 
         prediction_nodes.append(node)
     
--- tests/test_inference.py
+++ tests/test_inference.py
@@ -3,7 +3,7 @@ import pytest
 from hierarchicalsoftmax.nodes import SoftmaxNode, IndexNotSetError
 from hierarchicalsoftmax.inference import greedy_predictions, ShapeError
 
-def test_greedy_predictions():
+def get_root_and_targets():
     root = SoftmaxNode("root")
     a = SoftmaxNode("a", parent=root)
     aa = SoftmaxNode("aa", parent=a)
@@ -12,9 +12,13 @@ def test_greedy_predictions():
     ba = SoftmaxNode("ba", parent=b)
     bb = SoftmaxNode("bb", parent=b)
 
-    root.set_indexes()
-
     targets = [aa,ba,bb, ab]
+
+    return root, targets
+
+def test_greedy_predictions():
+    root, targets = get_root_and_targets()
+    root.set_indexes()
 
     predictions = torch.zeros( (len(targets), root.layer_size) )
     for target_index, target in enumerate(targets):
@@ -28,23 +32,36 @@ def test_greedy_predictions():
 
 
 def test_unset_indexes():
-    root = SoftmaxNode("root")
-    a = SoftmaxNode("a", parent=root)
-    aa = SoftmaxNode("aa", parent=a)
-    ab = SoftmaxNode("ab", parent=a)
-    b = SoftmaxNode("b", parent=root)
-    ba = SoftmaxNode("ba", parent=b)
-    bb = SoftmaxNode("bb", parent=b)
-
-    targets = [aa,ba,bb, ab]
+    root, targets = get_root_and_targets()
 
     predictions = torch.zeros( (len(targets), 10) )
 
     with pytest.raises(IndexNotSetError):
-        node_predictions = greedy_predictions(prediction_tensor=predictions, root=root)
+        greedy_predictions(prediction_tensor=predictions, root=root)
 
 
 def test_greedy_predictions():
+    root, targets = get_root_and_targets()
+
+    root.set_indexes()
+
+    predictions = torch.zeros( (len(targets), root.layer_size + 1) )
+    with pytest.raises(ShapeError):
+        node_predictions = greedy_predictions(prediction_tensor=predictions, root=root)
+
+
+def test_max_depth_simple():
+    root, targets = get_root_and_targets()
+
+    predictions = torch.zeros( (len(targets), root.layer_size) )
+    for target_index, target in enumerate(targets):
+        while target.parent:
+            predictions[ target_index, target.parent.softmax_start_index + target.index_in_parent ] = 20.0
+            target = target.parent
+    node_predictions = greedy_predictions(prediction_tensor=predictions, root=root, max_depth=1)
+    assert [str(node) for node in node_predictions] == ['a', 'b', 'b', 'a']
+
+def test_max_depth_complex():
     root = SoftmaxNode("root")
     a = SoftmaxNode("a", parent=root)
     aa = SoftmaxNode("aa", parent=a)
@@ -53,10 +70,25 @@ def test_greedy_predictions():
     ba = SoftmaxNode("ba", parent=b)
     bb = SoftmaxNode("bb", parent=b)
 
-    root.set_indexes()
+    aaa = SoftmaxNode("aaa", parent=aa)
+    aab = SoftmaxNode("aab", parent=aa)
+    aba = SoftmaxNode("aba", parent=ab)
+    abb = SoftmaxNode("abb", parent=ab)
+    
+    baa = SoftmaxNode("baa", parent=ba)
+    bab = SoftmaxNode("bab", parent=ba)
+    bba = SoftmaxNode("bba", parent=bb)
+    bbb = SoftmaxNode("bbb", parent=bb)
 
-    targets = [aa,ba,bb, ab]
+    targets = [aaa,aab,aba, abb, baa, bab, bba, bbb]
 
-    predictions = torch.zeros( (len(targets), root.layer_size + 1) )
-    with pytest.raises(ShapeError):
-        node_predictions = greedy_predictions(prediction_tensor=predictions, root=root)
+    predictions = torch.zeros( (len(targets), root.layer_size) )
+    for target_index, target in enumerate(targets):
+        while target.parent:
+            predictions[ target_index, target.parent.softmax_start_index + target.index_in_parent ] = 20.0
+            target = target.parent
+    node_predictions = greedy_predictions(prediction_tensor=predictions, root=root, max_depth=1)
+    assert [str(node) for node in node_predictions] == ['a', 'a', 'a', 'a', 'b', 'b', 'b', 'b']
+
+    node_predictions = greedy_predictions(prediction_tensor=predictions, root=root, max_depth=2)
+    assert [str(node) for node in node_predictions] == ['aa', 'aa', 'ab', 'ab', 'ba', 'ba', 'bb', 'bb']
